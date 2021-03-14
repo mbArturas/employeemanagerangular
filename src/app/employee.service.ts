d8ddd97a977cc6fe650e7037d70f877db5ba2c97
Arturas <mbarturas@yahoo.com>
2021-03-15 00:06:03 +0200

    Filling up Template

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -1,18 +1,19 @@
-import {Injectable} from "@angular/core";
-import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
-import {Employee} from "./employee";
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { Employee } from "./employee";
+import { environment } from "../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
-  private apiServerUrl = '';
+  private apiServerUrl = environment.apiBaseUrl;
 
-  constructor(private http: HttpClient) {  }
+  constructor(private http: HttpClient) { }
 
-  public getEmployees(): Observable<Employee> {
-    return this.http.get<Employee>(`${this.apiServerUrl}/employee/all`);
+  public getEmployees(): Observable<Employee[]> {
+    return this.http.get<Employee[]>(`${this.apiServerUrl}/employee/all`);
   }
 
   public addEmployees(employee: Employee): Observable<Employee> {
